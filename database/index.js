cdf54dafad96a89fe8d9b8a72a9bec324d493524
hendrikekon <hendrikekon25@gmail.com>
2025-01-22 09:33:23 +0700

    change db to mongodb atlas

--- database/index.js
+++ database/index.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const {dbHost, dbName, dbPort, dbUser, dbPass} = require('../app/config')
 
-const options = {
-    serverSelectionTimeoutMS: 60000, // Increase server selection timeout
-    socketTimeoutMS: 60000, // Increase socket timeout
-    authSource: 'admin', // Use the 'admin' database for authentication
-};
+// const options = {
+//     serverSelectionTimeoutMS: 60000, // Increase server selection timeout
+//     socketTimeoutMS: 60000, // Increase socket timeout
+//     authSource: 'admin', // Use the 'admin' database for authentication
+// };
 
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`, options);
+// mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`, options);
+// const db = mongoose.connection;
+
+// module.exports = db;
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.lh18l.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`);
 const db = mongoose.connection;
 
 module.exports = db;
-
